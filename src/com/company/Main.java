33275f70b1848e1952018acc0e23c3c47ffa279c
Craig Dunlop <craigdunlop@Craigs-MacBook-Air.local>
2021-09-28 19:53:07 +0100

    given a key shifts each letter by the specified number according to its order in the alphabet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,11 @@
 package com.company;
+
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         String message = "we found a treasure!";
         Scanner scanner = new Scanner(System.in);
         String originalText = scanner.nextLine();
