4941928043904614da19da16c91a8e90bd8597ee
Craig Dunlop <craigdunlop@Craigs-MacBook-Air.local>
2021-09-28 19:49:39 +0100

    given a key, shift the letters by the given numbers in the key

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,14 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         String message = "we found a treasure!";
-        String encrypted = Encryption.encrypt(message);
+        Scanner scanner = new Scanner(System.in);
+        String originalText = scanner.nextLine();
+        Integer key = scanner.nextInt();
+        String encrypted = Encryption.encrypt(originalText, key);
         System.out.println(encrypted);
     }
 }
